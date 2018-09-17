00c6f89edd3af13773d2bf006b8fea67902ddd02
7ma7X <7X.rusk@gmail.com>
2018-09-17 11:31:04 +0900

    :wrench: 標準入力で検索まで

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
+'use strict';
 const utasshot = require('./lib/utasshot');
 
 utasshot.shot();
-
-/*
-async function test () {
-    const puppeteer = require('puppeteer');
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.waitForResponse('https://twitter.com/hellorusk');
-    await browser.close();
-}
-
-test ();*/
